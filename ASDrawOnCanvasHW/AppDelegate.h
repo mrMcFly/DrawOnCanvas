5598ad4e24206e3af3d7cc076ef816741d1107ef
Alex Sergienko <Sergienko.a1986@gmail.com>
2015-03-13 20:27:16 +0200

    Initial Commit

---
+++ ASDrawOnCanvasHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASDrawOnCanvasHW
+//
+//  Created by Alex Sergienko on 13.03.15.
+//  Copyright (c) 2015 Alexandr Sergienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASDrawOnCanvasHW/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ASDrawOnCanvasHW
+//
+//  Created by Alex Sergienko on 13.03.15.
+//  Copyright (c) 2015 Alexandr Sergienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
